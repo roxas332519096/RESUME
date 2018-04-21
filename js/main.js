e2991d45ac37f784794078eb019cbf5bb2e7b368
roxasdeng <332519096@qq.com>
2018-04-21 20:27:47 +0800

    2027

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 setTimeout(function(){
-    stieWelcome.classList.remove('active')
-}, 3000);
+    stieWelcome.classList.remove('active')    
+},1000)
+
+window.onscroll = function(xxx){
+    if(window.scrollY > 0){
+        topNavBar.classList.add('sticky')
+    }else{
+        topNavBar.classList.remove('sticky')
+    }
+}
+
+let liTags = document.querySelectorAll('nav.menu > ul > li');
+for(let i=0; i<liTags.length; i++){
+  liTags[i].onmouseenter = function(x){
+    x.currentTarget.classList.add('active')
+  }
+  liTags[i].onmouseleave = function(x){
+    x.currentTarget.classList.remove('active')
+  }
+}
